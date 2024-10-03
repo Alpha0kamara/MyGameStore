461419aec5564e398395290b4deb700d2cb66a60
Alpha Kamara <94443900+Alpha0kamara@users.noreply.github.com>
2024-10-03 15:16:11 +0200

    a extensionmethod for seeding the db

--- MyGameStoreApi/MyGameStoreWebApi/DAL/GameStoreContext.cs
+++ MyGameStoreApi/MyGameStoreWebApi/DAL/GameStoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyGameStoreWebApi.DAL.ModelBuilderExtension;
 using MyGameStoreWebApi.Model;
 
 namespace MyGameStoreWebApi.DAL
@@ -13,55 +14,8 @@ namespace MyGameStoreWebApi.DAL
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfiguration(new PersonConfiguration());
             modelBuilder.ApplyConfiguration(new StoreConfiguration());
-            SeedData(modelBuilder);
+            //modelBuilder.SeedData();
         }
-        private void SeedData(ModelBuilder modelBuilder)
-        {
-            // seed stores
-            modelBuilder.Entity<Store>().HasData(
-                new Store { Id = 1, Name = "GameStore", Street = "GameStreet", Number = "1", Zipcode = "1234AB", City = "GameCity", IsFranchiseStore = false },
-                new Store { Id = 2, Name = "GymeStore", Street = "GymeStreet", Number = "2", Zipcode = "1234AB", City = "GymeCity", IsFranchiseStore = true }
-            );
-
-            // seed persons
-            modelBuilder.Entity<Person>().HasData(
-                new Person
-                {
-                    Id = 1,
-                    FirstName = "John",
-                    LastName = "Doe",
-                    Gender = 1, // Assuming 1 is for Male
-                    Email = "john.doe@example.com",
-                    StoreId = 1
-                },
-            new Person
-            {
-                Id = 2,
-                FirstName = "Jane",
-                LastName = "Smith",
-                Gender = 2, // Assuming 2 is for Female
-                Email = "jane.smith@example.com",
-                StoreId = 1
-            },
-            new Person
-            {
-                Id = 3,
-                FirstName = "Mike",
-                LastName = "Johnson",
-                Gender = 1,
-                Email = "mike.johnson@example.com",
-                StoreId = 2
-            },
-            new Person
-            {
-                Id = 4,
-                FirstName = "Emily",
-                LastName = "Brown",
-                Gender = 2,
-                Email = "emily.brown@example.com",
-                StoreId = 2
-            });
-        }
-
+        
     }
 }
---
+++ MyGameStoreApi/MyGameStoreWebApi/DAL/ModelBuilderExtension/SeedDataExtension.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyGameStoreWebApi.Model;
+
+namespace MyGameStoreWebApi.DAL.ModelBuilderExtension
+{
+    public static class SeedDataExtension
+    {
+        public static void SeedData(this ModelBuilder modelBuilder)
+        {
+            // seed stores
+            modelBuilder.Entity<Store>().HasData(
+                new Store { Id = 1, Name = "GameStore", Street = "GameStreet", Number = "1", Zipcode = "1234AB", City = "GameCity", IsFranchiseStore = false },
+                new Store { Id = 2, Name = "GymeStore", Street = "GymeStreet", Number = "2", Zipcode = "1234AB", City = "GymeCity", IsFranchiseStore = true }
+            );
+
+            // seed persons
+            modelBuilder.Entity<Person>().HasData(
+                new Person
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Gender = 1, // Assuming 1 is for Male
+                    Email = "john.doe@example.com",
+                    StoreId = 1
+                },
+            new Person
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Smith",
+                Gender = 2, // Assuming 2 is for Female
+                Email = "jane.smith@example.com",
+                StoreId = 1
+            },
+            new Person
+            {
+                Id = 3,
+                FirstName = "Mike",
+                LastName = "Johnson",
+                Gender = 1,
+                Email = "mike.johnson@example.com",
+                StoreId = 2
+            },
+            new Person
+            {
+                Id = 4,
+                FirstName = "Emily",
+                LastName = "Brown",
+                Gender = 2,
+                Email = "emily.brown@example.com",
+                StoreId = 2
+            });
+        }
+    }
+}
